164b248e4260abb9274706498e95e739ea17aac3
CABE <cabe@rocketmail.com>
2017-10-06 19:14:36 -0600

    enums

---
+++ RopeAssignment/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RopeAssignment
+{
+    /// <summary>
+    /// Position on the rope.
+    /// </summary>
+    public enum Position
+    { //only 3 will be visible.
+        RightStartPoint = 4,
+        AlmostRight = 3,
+        Middle = 2,
+        AlmostLeft = 1,
+        LeftStartPoint = 0
+    }
+}
