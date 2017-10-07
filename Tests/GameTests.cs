b70daf40ebd638f4190382d8fb8339811d946de1
CABE <cabe@rocketmail.com>
2017-10-06 19:36:12 -0600

    unit tests

--- Tests/GameTests.cs
+++ Tests/GameTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using RopeAssignment;
+using System.Linq;
 namespace Tests
 {
     [TestClass]
@@ -14,7 +15,21 @@ namespace Tests
         {
             //The rope can hold a maximum of 3 monkeys at a time.
 
-            Assert.Fail("Placeholder");
+            IGame game = new Game(4, 0);
+            game.ProcessMoves();
+            Assert.IsTrue(game.CreaturesOnRopeCount == 1);
+            game.ProcessMoves();
+            Assert.IsTrue(game.CreaturesOnRopeCount == 2);
+            game.ProcessMoves();
+            Assert.IsTrue(game.CreaturesOnRopeCount == 3);
+            game.ProcessMoves();
+            Assert.IsTrue(game.CreaturesOnRopeCount == 3);
+            game.ProcessMoves();
+            Assert.IsTrue(game.CreaturesOnRopeCount == 2);
+
+
+
+
         }
         /// <summary>
         /// This tests the number of steps it takes to cross the rope.
@@ -23,7 +38,15 @@ namespace Tests
         public void RopeNumberOfSteps()
         {
             //Each monkey requires 4 steps to cross the rope. ONLY THREE ARE VISIBLE.
-            Assert.Fail("Placeholder");
+            IGame game = new Game(1, 0);
+            game.ProcessMoves();
+            Assert.IsTrue(game.LeftCreatureCount == 1);
+            game.ProcessMoves();
+            Assert.IsTrue(game.LeftCreatureCount == 1);
+            game.ProcessMoves();
+            Assert.IsTrue(game.LeftCreatureCount == 1);
+            game.ProcessMoves();
+            Assert.IsTrue(game.LeftCreatureCount == 0);
         }
         /// <summary>
         /// This test determines whether or not a monkey is getting impatient.
@@ -32,7 +55,23 @@ namespace Tests
         public void Impatience()
         {
             //Make sure the monkey at the front of the line doesn't get impatient.
-            Assert.Fail("Placeholder");
+            IGame game = new Game(4, 2);
+            for (int x = 0; x < 3; x++)
+            {
+                game.ProcessMoves();
+            }
+            Assert.IsTrue(game.CreaturesOnRopeCount == 3);
+            Assert.IsTrue(game.LeftCreatures.Count(l => l.Position == Position.LeftStartPoint) == 1);
+            Assert.IsTrue(game.RightCreatures.All(r => r.Position == Position.RightStartPoint));
+            game.ProcessMoves();
+            Assert.IsTrue(game.LeftCreatures.Count(l => l.Position == Position.LeftStartPoint) == 1);
+            Assert.IsTrue(game.LeftCreatures.Count(l => l.Position == Position.AlmostLeft) == 0);
+            game.ProcessMoves();
+            Assert.IsTrue(game.LeftCreatures.Count(l => l.Position == Position.Middle) == 0);
+            game.ProcessMoves();
+            Assert.IsTrue(game.LeftCreatures.Count(l => l.Position == Position.AlmostRight) == 0);
+            Assert.IsTrue(game.RightCreatures.Count(r => r.Position == Position.AlmostRight) == 1);
+
         }
         /// <summary>
         /// This test makes sure that the monkey at the other end of the chasm is halted when there are others crossing.
@@ -41,7 +80,14 @@ namespace Tests
         public void MonkeysHaltedOnOtherSide()
         {
             //Once a monkey starts across the rope, the monkeys on the other side of the rope are not allowed to begin crossing.
-            Assert.Fail("Placeholder");
+            IGame game = new Game(4, 2);
+            game.ProcessMoves();
+            Assert.IsTrue(game.RightCreatures.All(r => r.Position == Position.RightStartPoint));
+            game.ProcessMoves();
+            Assert.IsTrue(game.RightCreatures.All(r => r.Position == Position.RightStartPoint));
+            game.ProcessMoves();
+            Assert.IsTrue(game.RightCreatures.All(r => r.Position == Position.RightStartPoint));
+
         }
         /// <summary>
         /// This tests the ability to add a monkey/creature.
@@ -49,7 +95,11 @@ namespace Tests
         [TestMethod]
         public void AddMonkey()
         {
-            Assert.Fail("Placeholder");
+            IGame game = new Game(0, 0);
+            Assert.IsTrue(game.LeftCreatureCount == 0 && (game.LeftCreatures == null || game.LeftCreatures.Count() == 0));
+            game.AddCreature(Orientation.LeftGoingRight);
+            Assert.IsTrue(game.LeftCreatureCount == 1 && (game.LeftCreatures == null || game.LeftCreatures.Count() == 1));
+
         }
 
         /// <summary>
@@ -58,6 +108,7 @@ namespace Tests
         [TestMethod]
         public void InjectableGameTest()
         {
+            //ill get to this one soon.
             Assert.Fail("Placeholder");
         }
 
